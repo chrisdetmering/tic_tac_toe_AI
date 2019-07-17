9ba01ec658d20427a9db59694609d6ac9478d8ac
froggy <chrisdetmering@gmail.com>
2019-07-17 15:04:45 -0700

    Made children instance method

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -1,24 +1,55 @@
 require_relative 'tic_tac_toe'
 
+
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
-    @board = board 
-    @next_mover_mark = next_mover_mark
-    @prev_move_pos = prev_move_pos
+    @board, @next_mover_mark, @prev_move_pos = 
+    board, next_mover_mark, prev_move_pos
   end
 
-  attr_accessor :board, :next_mover_mark
+  attr_accessor :board, :next_mover_mark, :prev_move_pos
  
+  
+
+
+
 
   def losing_node?(evaluator)
+    self.board.over? 
+
+
   end
 
   def winning_node?(evaluator)
+
+
+
+
   end
 
-  
+
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+      children = []
+
+    (0..2).each do |x| 
+      (0..2).each do |y| 
+
+          pos = [x, y]
+
+        next unless board.empty?(pos)
+        duped_board = board.dup 
+
+        duped_board[pos] = self.next_mover_mark
+        new_mark = (self.next_mover_mark == :x ? :o : :x)
+
+        node = TicTacToeNode.new(duped_board, new_mark, pos)
+
+        children << node
+      end 
+    end   
+
+    children 
   end
 end
