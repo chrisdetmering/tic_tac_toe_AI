00e63c84bdb50384568ad6e4ad7df73d6abf44c8
froggy <chrisdetmering@gmail.com>
2019-07-17 17:23:57 -0700

    Started the supercomputerplayer class

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,9 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+      node = TicTacToeNode.new(game, mark)
+
+      node.children
   end
 end
 
